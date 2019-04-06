9fd95f0c2d6edd69f270f568cf059d7f442d9eec
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2019-04-06 19:49:38 +0300

    Tabs to spaces

--- src/typesetter.js
+++ src/typesetter.js
@@ -1,105 +1,105 @@
 (function (w) {
-	'use strict';
-	
-	var KEY_SPACE = 32,
-		KEY_ENTER = 13,
-		KEY_TAB = 9,
-		KEY_BACKSPACE = 8,
-		KEY_LINEBREAK = 10,
+    'use strict';
 
-		App = {
-			targetElement: null,
-			settings: {},
-			expectations: [],
-			currentPosition: 0,
-		};
+    var KEY_SPACE = 32,
+        KEY_ENTER = 13,
+        KEY_TAB = 9,
+        KEY_BACKSPACE = 8,
+        KEY_LINEBREAK = 10,
 
-	w.typesetter = function (element, options) {
-		App.targetElement = element;
-		App.settings = options;
+        App = {
+            targetElement: null,
+            settings: {},
+            expectations: [],
+            currentPosition: 0,
+        };
 
-		init();
-	};
+    w.typesetter = function (element, options) {
+        App.targetElement = element;
+        App.settings = options;
 
-	function init() {
-		App.targetElement.innerHTML = pharseToHtml(App.settings.data);
+        init();
+    };
 
-		setClassToSymbol(App.currentPosition, 'cursor');
+    function init() {
+        App.targetElement.innerHTML = pharseToHtml(App.settings.data);
 
-		attachEventListeners();
-	}
+        setClassToSymbol(App.currentPosition, 'cursor');
 
-	function pharseToHtml(text) {
-		var i = 0,
-			html = '',
-			symbol = '';
+        attachEventListeners();
+    }
 
-		for (i = 0; i < text.length; i++) {
-			symbol = text[i].charCodeAt(0);
-			switch (symbol) {
-			case KEY_LINEBREAK:
-				html += '<span class="break-line"><br></span>';
-				App.expectations.push(KEY_ENTER);
-				break;
-			case KEY_TAB:
-				html += "<span> </span><span> </span><span> </span><span> </span>";
-				App.expectations.push(KEY_SPACE, KEY_SPACE, KEY_SPACE, KEY_SPACE);
-				break;
-			default:
-				html += "<span>" + text[i] + "</span>";
-				App.expectations.push(symbol);
-				break;
-			}
-		}
+    function pharseToHtml(text) {
+        var i = 0,
+            html = '',
+            symbol = '';
 
-		return html;
-	}
+        for (i = 0; i < text.length; i++) {
+            symbol = text[i].charCodeAt(0);
+            switch (symbol) {
+            case KEY_LINEBREAK:
+                html += '<span class="break-line"><br></span>';
+                App.expectations.push(KEY_ENTER);
+                break;
+            case KEY_TAB:
+                html += "<span> </span><span> </span><span> </span><span> </span>";
+                App.expectations.push(KEY_SPACE, KEY_SPACE, KEY_SPACE, KEY_SPACE);
+                break;
+            default:
+                html += "<span>" + text[i] + "</span>";
+                App.expectations.push(symbol);
+                break;
+            }
+        }
 
-	function attachEventListeners() {
+        return html;
+    }
 
-		w.addEventListener('keydown', function (event) {
-			var keyCode = event.keyCode || event.which;
-			if (keyCode === KEY_TAB || keyCode === KEY_BACKSPACE) {
-				event.preventDefault();
-			}
-		});
-	
-		w.addEventListener('keypress', function (event) {
-			event.preventDefault();
+    function attachEventListeners() {
 
-			var keyCode = event.keyCode || event.which;
+        w.addEventListener('keydown', function (event) {
+            var keyCode = event.keyCode || event.which;
+            if (keyCode === KEY_TAB || keyCode === KEY_BACKSPACE) {
+                event.preventDefault();
+            }
+        });
 
-			if (App.expectations[App.currentPosition] === keyCode) {
-				setClassToSymbol(App.currentPosition, 'done-symbol');
+        w.addEventListener('keypress', function (event) {
+            event.preventDefault();
 
-				if (keyCode === KEY_ENTER) {
-					while (App.expectations[App.currentPosition + 1] === KEY_SPACE) {
-						App.currentPosition++;
-					}
-				}
-				App.currentPosition++;
+            var keyCode = event.keyCode || event.which;
 
-				setClassToSymbol(App.currentPosition, 'cursor');
-			} else {
-				setClassToSymbol(App.currentPosition, 'cursor-exeption');
-			}
+            if (App.expectations[App.currentPosition] === keyCode) {
+                setClassToSymbol(App.currentPosition, 'done-symbol');
 
-		});
-	}
+                if (keyCode === KEY_ENTER) {
+                    while (App.expectations[App.currentPosition + 1] === KEY_SPACE) {
+                        App.currentPosition++;
+                    }
+                }
+                App.currentPosition++;
 
-	function setClassToSymbol(symbolIndex, className) {
-		var symbol = getSymbolByIndex(symbolIndex);
-		var oldClass = symbol.className;
+                setClassToSymbol(App.currentPosition, 'cursor');
+            } else {
+                setClassToSymbol(App.currentPosition, 'cursor-exeption');
+            }
 
-		if (oldClass.indexOf('break-line') > -1 && className.indexOf('cursor') > -1) {
-			symbol.className = oldClass + ' ' + className;
-		} else {
-			symbol.className = className;
-		}
-	}
+        });
+    }
 
-	function getSymbolByIndex(index) {
-		return App.targetElement.querySelectorAll('span')[index];
-	}
+    function setClassToSymbol(symbolIndex, className) {
+        var symbol = getSymbolByIndex(symbolIndex);
+        var oldClass = symbol.className;
+
+        if (oldClass.indexOf('break-line') > -1 && className.indexOf('cursor') > -1) {
+            symbol.className = oldClass + ' ' + className;
+        } else {
+            symbol.className = className;
+        }
+    }
+
+    function getSymbolByIndex(index) {
+        return App.targetElement.querySelectorAll('span')[index];
+    }
 
 }(window));
